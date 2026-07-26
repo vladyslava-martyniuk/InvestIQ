568eaccfbdd33720d7247d557280f692e4e7765d
vladyslava-martyniuk <vlada.martyniuk@gmail.com>
2026-07-26 10:33:09 +0300

    FIXED FIREBASE

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
+console.log(import.meta);
 const firebaseConfig = {
-  apiKey: "AIzaSyALgMftnulLoguJKoP0SpBlD37nxqmPh_E",
-  authDomain: "investqi-b751e.firebaseapp.com",
-  projectId: "investqi-b751e",
-  storageBucket: "investqi-b751e.firebasestorage.app",
-  messagingSenderId: "43505719333",
-  appId: "1:43505719333:web:e80450c4df0b57840b87cc",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
+export const googleProvider = new GoogleAuthProvider();
+console.log(firebaseConfig);
